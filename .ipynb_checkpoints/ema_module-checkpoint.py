5209909229ca742a4ece5006928f2470110a4764
Andrew Cole <andrewcole.817@gmail.com>
2020-07-14 12:49:48 -0500

    Add Rolling Average Plot Function

--- .ipynb_checkpoints/ema_module-checkpoint.py
+++ .ipynb_checkpoints/ema_module-checkpoint.py
@@ -6,6 +6,7 @@ import matplotlib.dates as mdates
 import seaborn as sns
 from functools import reduce
 import datetime
+import numpy as np
 
 sns.set(style = 'darkgrid', context = 'talk', palette = 'Dark2')
 
@@ -14,15 +15,26 @@ warnings.filterwarnings('ignore')
 
 
 
-def grab_portfolio_data(tickers, start, end, interval, attribute, short_window, long_window):
+def grab_portfolio_data(tickers, start, end, interval, attribute):
     
     yf.pdr_override()
     date_format = mdates.DateFormatter('%m/%y')
     df = pdr.get_data_yahoo(tickers, start = start, end = end, interval = interval)
     df = df[f"{attribute}"]
     
+    return df
+    
+    
+    
+
+def view_portfolio_history(df, ticker, short_window, long_window):
+    
     short_rolling = df.rolling(window = short_window).mean()
     long_rolling = df.rolling(window = long_window).mean()
     
+    fig, ax = plt.subplots(figsize = (20, 9))
+    plot = sns.lineplot(df.index, df[f"{ticker}"], label = f"{ticker} Close Price")
+    plot = sns.lineplot(short_rolling.index, short_rolling[f"{ticker}"], label = f"{ticker} {short_window}-day SMA")
+    plot = sns.lineplot(long_rolling.index, long_rolling[f"{ticker}"], label = f"{ticker} {long_window}-day SMA")
     
-    
+    return plot
